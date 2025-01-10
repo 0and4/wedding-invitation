2fd0fb55e036fa0bd805bb4c18815e50283967a1
SeHee <say42481181@gmail.com>
2025-01-10 15:01:26 +0900

    Save current changes before rebase

--- src/component/ui/Wrapper.js
+++ src/component/ui/Wrapper.js
@@ -5,5 +5,9 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+<<<<<<< HEAD
+=======
+  justify-content: center;
+>>>>>>> e9b9919 (Save current changes before rebase)
   text-align: center;
 `;
