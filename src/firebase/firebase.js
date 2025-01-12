f8908ee312de943192a392ae169873cb9465edfa
0and4 <iwonchu215@gmail.com>
2025-01-12 14:58:17 +0900

    firebase 환경변수 설정

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,14 +8,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAai_VYPTela-p-7NszEZshp0gYworBR9g",
-  authDomain: "wedding-snb.firebaseapp.com",
-  databaseURL: "https://wedding-snb-default-rtdb.firebaseio.com",
-  projectId: "wedding-snb",
-  storageBucket: "wedding-snb.firebasestorage.app",
-  messagingSenderId: "981317826433",
-  appId: "1:981317826433:web:83d3e9ce7d388d456ada80",
-  measurementId: "G-55VBH1WG7Z",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MESUREMENT_ID,
 };
 
 // Initialize Firebase
